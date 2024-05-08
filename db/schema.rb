ef69e6fc186a74fabb893f3a890fe8c3ddc43fb5
yume-ebina <cityeventa@gmail.com>
2024-05-08 17:45:50 +0900

    feat:画像をスライドで表示できるようにしました、userモデル作成しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_25_101226) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_26_110437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_25_101226) do
     t.text "description"
     t.integer "price"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "name", null: false
+    t.string "email"
+    t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
