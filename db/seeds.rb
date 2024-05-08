9d5fb952794e9fc48eb99c0e1703488d4bb33566
yume-ebina <cityeventa@gmail.com>
2024-05-08 21:09:03 +0900

    feat:デモユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,43 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  provider: "hoge",
+  uid: "hoge",
+  name: "test",
+  email: "aaaaaaaa@hoge.com"
+)
+
 Product.create(
-[
-  {
-    name: "オリジナルブレンド",
-    description: "バランスの良いブレンドです",
-    price: 700,
-    status: 1
-  },
-  {
-    name: "モカブレンド",
-    description: "少し酸味のあるブレンドです",
-    price: 700,
-    status: 1
-  },
-  {
-    name: "マンデリンブレンド",
-    description: "苦味とコクが特徴のブレンドです",
-    price: 700,
-    status: 1
-  },
-  {
-    name: "キリマンジャロブレンド",
-    description: "優しい酸味とフルーティーさのあるブレンドです",
-    price: 700,
-    status: 1
-  },
-  {
-    name: "デカフェ",
-    description: "デカフェのシングルオリジンです",
-    price: 700,
-    status: 1
-  },
-  {
-    name: "オーガニック",
-    description: "オーガニックのシングルオリジンです",
-    price: 900,
-    status: 1
-  },
-]
+  [
+    {
+      name: "オリジナルブレンド",
+      description: "バランスの良いブレンドです",
+      price: 700,
+      status: 1
+    },
+    {
+      name: "モカブレンド",
+      description: "少し酸味のあるブレンドです",
+      price: 700,
+      status: 1
+    },
+    {
+      name: "マンデリンブレンド",
+      description: "苦味とコクが特徴のブレンドです",
+      price: 700,
+      status: 1
+    },
+    {
+      name: "キリマンジャロブレンド",
+      description: "優しい酸味とフルーティーさのあるブレンドです",
+      price: 700,
+      status: 1
+    },
+    {
+      name: "デカフェ",
+      description: "デカフェのシングルオリジンです",
+      price: 700,
+      status: 1
+    },
+    {
+      name: "オーガニック",
+      description: "オーガニックのシングルオリジンです",
+      price: 900,
+      status: 1
+    },
+  ]
 )
